c7cd37a9dbc62fec3c2a3a2d910e43677a044887
robin.singla <robinformal@gmail.com>
2021-04-28 07:18:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gridview/GridAdapter.java
@@ -0,0 +1,60 @@
+package com.example.gridview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class GridAdapter extends BaseAdapter {
+
+    Context context;
+    String[] flowerName;
+    int[] image;
+
+    LayoutInflater inflater;
+
+    public GridAdapter(Context context, String[] flowerName, int[] image) {
+        this.context = context;
+        this.flowerName = flowerName;
+        this.image = image;
+    }
+
+    @Override
+    public int getCount() {
+        return flowerName.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+       if (inflater == null)
+           inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+       if (convertView == null){
+
+           convertView = inflater.inflate(R.layout.grid_item,null);
+
+       }
+
+        ImageView imageView = convertView.findViewById(R.id.grid_image);
+        TextView textView = convertView.findViewById(R.id.item_name);
+
+        imageView.setImageResource(image[position]);
+        textView.setText(flowerName[position]);
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/gridview/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.gridview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.example.gridview.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String[] flowerName = {"Rose","Lotus","Lily","Jasmine",
+        "Tulip","Orchid","Levender","RoseMarry","Sunflower","Carnation"};
+        int[] flowerImages = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,R.drawable.f,R.drawable.g,
+                R.drawable.h,R.drawable.i,R.drawable.j};
+
+        GridAdapter gridAdapter = new GridAdapter(MainActivity.this,flowerName,flowerImages);
+        binding.gridView.setAdapter(gridAdapter);
+
+
+        binding.gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(MainActivity.this,"You Clicked on "+ flowerName[position],Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
+}
